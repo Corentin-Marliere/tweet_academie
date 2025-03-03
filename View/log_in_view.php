40593df4f97a036889592503961ab7d93ebc26fa
BucheronV <dhapvirgil1401@gmail.com>
2025-03-03 16:24:12 +0100

    ajout d'image

--- View/log_in_view.php
+++ View/log_in_view.php
@@ -1,1 +1,13 @@
-<?="COUCOU"?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FACCOVI</title>
+</head>
+<body>
+    <header>
+        <img src="../Assets/logo/logo.png" alt="">
+    </header>
+</body>
+</html>
