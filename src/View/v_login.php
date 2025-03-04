7557f6ab3c42e5b2ab775a52ca7ee90bed6d2d8f
Corentin.M <corentin.marliere@epitech.eu>
2025-03-04 10:36:47 +0100

    [RELEASE] Modification files and folder for better architecture

---
+++ src/View/v_login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include('../_partial/_head.php') ?>
+    <title>Facovi - Connexion</title>
+</head>
+
+<body>
+    <header>
+        <?php include('../_partial/_header.php') ?>
+    </header>
+
+    <main>
+        <div>
+            <h2>Hello World</h2>
+        </div>
+    </main>
+
+    <footer>
+        <?php include('../_partial/_footer.php') ?>
+    </footer>
+</body>
+
+</html>
