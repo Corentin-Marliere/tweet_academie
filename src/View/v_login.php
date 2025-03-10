aa27673b577f1bea9e29332262681fdbd90fd42c
Corentin.M <corentin.marliere@epitech.eu>
2025-03-10 17:28:36 +0100

    [update] modification of button

--- src/View/v_login.php
+++ src/View/v_login.php
@@ -15,17 +15,17 @@
   <main>
     <div class="flex flex-col text-center m-5">
       <form>
-        <div class="flex flex-col items-center mb-3">
-          <label for="Mail" class="underline">Entrer votre Email: </label>
-          <input type="Email" name="Email" required
-              class="border w-48 p-1">
+        <div class="flex flex-col items-center mb-4">
+          <label for="Mail" class="underline mb-2">Entrer votre Email: </label>
+          <input type="Email" name="Email" placeholder="mon.adresse@mail.com" required
+              class="border rounded-lg w-72 p-1.5">
         </div>
-        <div class="flex flex-col items-center mb-3">
-          <label for="MDP" class="underline"> Entrer votre Mot de Passe: </label>
-          <input type="Password" name="Password" required
-              class="border w-48 p-1"></input>
+        <div class="flex flex-col items-center mb-5">
+          <label for="MDP" class="underline mb-2"> Entrer votre Mot de Passe: </label>
+          <input type="Password" name="Password" placeholder="Mot de passe" required
+              class="border rounded-md w-72 p-1.5"></input>
         </div>
-        <input type="submit" value="Se connecter"></input>
+        <input type="submit" value="Se connecter" class="rounded-lg pt-2 pr-8 pb-2 pl-8 bg-blue-600"></input>
       </form>
       <div class="m-5">
         <p>Pas de compte?</p>
