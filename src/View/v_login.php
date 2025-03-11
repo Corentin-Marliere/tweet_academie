2954972a4705eb7e74b3c53a2c854ec779a21ca4
Corentin.M <corentin.marliere@epitech.eu>
2025-03-11 16:51:04 +0100

    [UPDATE] Add changement of login page CSS

--- src/View/v_login.php
+++ src/View/v_login.php
@@ -8,20 +8,21 @@
 </head>
 
 <body>
-  <header>
-    <?php include('../_partial/_header.php') ?>
+  <header class="flex justify-center">
+    <img class="w-48" src="../../public/Assets/logo/logo.png" alt="">
   </header>
 
   <main>
+    <h1 class="text-center font-bold text-2xl mb-12">Connectez vous !</h1>
     <div class="flex flex-col text-center m-5">
-      <form>
+      <form class="mb-5">
         <div class="flex flex-col items-center mb-4">
-          <label for="Mail" class="underline mb-2">Entrer votre Email: </label>
+          <label for="Mail" class="underline mb-2">Entrer votre Email : </label>
           <input type="Email" name="Email" placeholder="mon.adresse@mail.com" required
               class="border rounded-lg w-72 p-1.5">
         </div>
         <div class="flex flex-col items-center mb-5">
-          <label for="MDP" class="underline mb-2"> Entrer votre Mot de Passe: </label>
+          <label for="MDP" class="underline mb-2"> Entrer votre Mot de Passe : </label>
           <input type="Password" name="Password" placeholder="Mot de passe" required
               class="border rounded-md w-72 p-1.5"></input>
         </div>
@@ -29,7 +30,7 @@
       </form>
       <div class="m-5">
         <p>Pas de compte?</p>
-        <a href="../View/v_register.php" class="text-blue-500 underline">Inscrivez-vous ici!</a>
+        <a href="../View/v_register.php" class="text-blue-500 border-blue-800 underline">Inscrivez-vous ici!</a>
       </div>
     </div>
   </main>
