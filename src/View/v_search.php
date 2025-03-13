eb754a3641843caf73f0cd997a301539b5f8cf95
Lughleur <fabien.picavez@epitech.eu>
2025-03-13 11:03:00 +0100

    [UPDATE] Search page finished

---
+++ src/View/v_search.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Faccovi - Page de recherche</title>
+</head>
+<body>
+<header>
+<a href="#"><img src="#" alt="#"></a> <!--profile picture-->
+<img src="../../public/Assets/logo/short_logo.png">
+<div>
+    <label for="Search">Entrez votre recherche</label>
+    <input type="Text" name="Search">
+</div>
+</header>
+<main>
+<h2>Dernière recherche de profile:</h2>
+<div>
+  <a href="#"><img src="#" alt="#"></a><!--redirection sur profile-->
+  <h6>@#</h6>
+  <a href="#"><img src="#" alt="#"></a>
+  <h6>@#</h6>
+  <a href="#"><img src="#" alt="#"></a>
+  <h6>@#</h6>
+  <a href="#"><img src="#" alt="#"></a>
+  <h6>@#</h6>
+</div>
+<hr>
+<div>
+  <h2>Mots clés recherché récemment:</h2>
+  <h5>#</h5>
+  <h5>#</h5>
+  <h5>#</h5>
+  <h5>#</h5>
+</div>
+<form>
+  <input type="submit" value="+"></input>
+  </form>
+</main>
+</body>
+
+</html>
