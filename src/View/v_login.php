85775f53e4dfcb942932453f530e248a2b33867f
Corentin.M <corentin.marliere@epitech.eu>
2025-03-14 11:27:54 +0100

    [update] finished the style of login page

--- src/View/v_login.php
+++ src/View/v_login.php
@@ -10,36 +10,38 @@
   <title>Faccovi - Connexion</title>
 </head>
 
-<body class="flex flex-col" data-theme="wintry">
+<body class="flex flex-col min-h-screen" data-theme="wintry">
   <header class="flex flex-row justify-center">
     <img src="../../public/Assets/logo/logo.png" alt=""
       class="w-44">
   </header>
 
-  <main class="flex flex-col">
-    <div class="flex flex-row justify-around items-center mb-5">
-      <h1>Connectez vous !</h1>
-      <button id="toogle_Theme" type="button" class="btn-icon variant-filled">test</button>
+  <main class="flex flex-col grow justify-center">
+    <div class="flex flex-row justify-center items-center mb-5">
+      <h1 class="text-xl mr-5">Connectez vous !</h1>
+      <button id="toogle_Theme" type="button" class="btn-icon variant-ghost-surface w-auto ml-5">
+        <img src="../../public/Assets/images/brightness-and-contrast.png" alt=""
+          class="w-5"></button>
     </div>
     <div>
       <div class="flex flex-col items-center">
         <form>
-          <div>
+          <div class="mb-5">
             <label for="Mail" class="underline mb-0.5">Email : </label>
             <input type="Email" name="Email" placeholder="Entrez votre adresse mail"
               class="input" required>
           </div>
-          <div>
-            <label for="MDP" class="underline">Mot de Passe : </label>
+          <div class="mb-5">
+            <label for="MDP" class="underline mb-0.5">Mot de Passe : </label>
             <input type="Password" name="Password" placeholder="Entrez votre mot de passe"
               class="input" required>
           </div>
-          <div class="text-center">
+          <div class="text-center mb-5">
             <input type="submit" value="Se connecter" class="btn btn-sm w-36 bg-gradient-to-br variant-gradient-tertiary-secondary">
-          </divu>
+            </div>
         </form>
       </div>
-      <div class="text-center">
+      <div class="text-center m-5">
         <p>Pas de compte ?</p>
         <a href="../View/v_register.php" class="underline text-blue-500">Inscrivez-vous ici</a>
       </div>
