bb167624d7bc0136693a99ce8c7c4ac596c44518
Corentin.M <corentin.marliere@epitech.eu>
2025-03-14 16:22:25 +0100

    [update] finished the header partial

--- src/View/v_home.php
+++ src/View/v_home.php
@@ -1,36 +1,34 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="fr" class="light">
 
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../../dist/styles.css">
+
   <title>Faccovi - Page d'acceuil</title>
 </head>
-<body>
 
-<header>
-  <a href=""><img src="" alt="#"></a>  <!--NPO changé les liens-->
-  <img src="../../public/Assets/logo/short_logo.png">
-  <a href="../View/v_search.php" alt="redirection_to_search_page">SR</a>
-</header>
+<body data-theme="modern">
+  <?php include('../_partial/_header.php') ?>
 
-<main>
+  <main>
 
-  <h1>Fil d'actualité</h1>
-  <a href=""><img src="" alt=""></a> <!--NPO changé les liens search button-->
-  <a href=""><img src="" alt=""></a> <!-- message button to go into dms-->
+    <h1>Fil d'actualité</h1>
+    <a href=""><img src="" alt=""></a> <!--NPO changé les liens search button-->
+    <a href=""><img src="" alt=""></a> <!-- message button to go into dms-->
 
-  <?php include('../_partial/_tweet.php') ?>
+    <?php include('../_partial/_tweet.php') ?>
 
-  <?php include('../_partial/_tweet.php') ?>
+    <?php include('../_partial/_tweet.php') ?>
 
-  <?php include('../_partial/_tweet.php') ?>
+    <?php include('../_partial/_tweet.php') ?>
 
-  <form>
-  <input type="submit" value="+"></input>
-  </form>
+    <form>
+      <input type="submit" value="+"></input>
+    </form>
 
-</main>
+  </main>
 </body>
 
 </html>
