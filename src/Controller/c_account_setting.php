11e10b0daeadc91791986cb48dc98e8e41b96c1d
BucheronV <dhapvirgil1401@gmail.com>
2025-03-16 15:29:27 +0100

    [UPDATE] update account setting

--- src/Controller/c_account_setting.php
+++ src/Controller/c_account_setting.php
@@ -2,3 +2,16 @@
 session_start();
 require('../Model/m_register.php');
 $bdd = new Model();
+
+$displayName = trim($_POST['new-display-name']);
+
+if (isset($_POST['new-bio'])) {
+	$bio = trim($_POST['new-bio']);
+} else {
+	$bio = null;
+}
+
+$userId = $_SESSION['user_id'];
+
+$updateAccount = $bdd->updateAccount($displayName, $bio, $userId);
+header('location: ../Controller/c_account.php');
